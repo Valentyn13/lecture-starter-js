76d96db572e2f03735075e1cb2af3beda560d52d
Valentyn13 <valentyn13git@gmail.com>
2023-12-12 18:47:00 +0200

    create main logic of tekken app

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,22 +1,184 @@
 import controls from '../../constants/controls';
 
-export async function fight(firstFighter, secondFighter) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(firstFighter);
-        }, 3000);
-        // resolve the promise with the winner when fight is over
-    });
+const {
+    PlayerOneAttack,
+    PlayerOneBlock,
+    PlayerTwoAttack,
+    PlayerTwoBlock,
+    PlayerOneCriticalHitCombination,
+    PlayerTwoCriticalHitCombination
+} = controls;
+
+export function criticalHitChance() {
+    return Math.random() + 1;
 }
 
-export function getDamage(attacker, defender) {
-    // return damage
+export function dodgeChance() {
+    return Math.random() + 1;
 }
-
 export function getHitPower(fighter) {
-    // return hit power
+    return fighter.attack * criticalHitChance();
 }
 
 export function getBlockPower(fighter) {
-    // return block power
+    return fighter.defense;
+}
+
+export function criticalHit(fighter) {
+    return fighter.attack * 2;
+}
+
+export function getDamage(attacker, defender, isBlockExist) {
+    let attackPower;
+
+    if (isBlockExist) {
+        attackPower = getHitPower(attacker) - getBlockPower(defender) * dodgeChance();
+    } else {
+        attackPower = getHitPower(attacker);
+    }
+
+    if (attackPower < 0) {
+        return 0;
+    }
+
+    return attackPower;
+}
+
+let firstFighterBlockActive = false;
+let secondFighterBlockActive = false;
+
+let criticalAttackDelayOneEnd = true;
+let criticalAttackDelayTwoEnd = true;
+
+function isCriticalAttackReady() {
+    const keysStatuses = Object.values(this);
+    const isCombinationNotReady = keysStatuses.includes(false);
+    if (isCombinationNotReady) {
+        return false;
+    }
+    return true;
+}
+
+const firstFighterCriticalAttackKeys = {
+    [PlayerOneCriticalHitCombination[0]]: false,
+    [PlayerOneCriticalHitCombination[1]]: false,
+    [PlayerOneCriticalHitCombination[2]]: false
+};
+
+const secondFighterCriticalAttackKeys = {
+    [PlayerTwoCriticalHitCombination[0]]: false,
+    [PlayerTwoCriticalHitCombination[1]]: false,
+    [PlayerTwoCriticalHitCombination[2]]: false
+};
+
+const fighterOneCriticalAttackReady = isCriticalAttackReady.bind(firstFighterCriticalAttackKeys);
+const fighterTwoCriticalAttackReady = isCriticalAttackReady.bind(secondFighterCriticalAttackKeys);
+
+export async function fight(firstFighter, secondFighter) {
+    let firstFighterHealth = firstFighter.health;
+    let secondFighterHealth = secondFighter.health;
+
+    const leftFighterIndicator = document.getElementById('left-fighter-indicator');
+    const rightFighterIndicator = document.getElementById('right-fighter-indicator');
+
+    return new Promise(resolve => {
+        document.addEventListener('keydown', e => {
+            switch (e.code) {
+                case PlayerOneAttack: // A
+                    if (!firstFighterBlockActive) {
+                        const damage = getDamage(firstFighter, secondFighter, secondFighterBlockActive);
+                        console.log(`Damage: ${damage}, isBlockExist: ${secondFighterBlockActive}`);
+                        secondFighterHealth -= damage;
+                    }
+
+                    break;
+                case PlayerTwoAttack: // J
+                    if (!secondFighterBlockActive) {
+                        const damage = getDamage(secondFighter, firstFighter, firstFighterBlockActive);
+                        console.log(`Damage: ${damage}, isBlockExist: ${secondFighterBlockActive}`);
+                        firstFighterHealth -= damage;
+                    }
+                    break;
+
+                case PlayerOneBlock: // D
+                    firstFighterBlockActive = true;
+                    break;
+
+                case PlayerTwoBlock: // L
+                    secondFighterBlockActive = true;
+                    break;
+
+                case PlayerOneCriticalHitCombination[0]:
+                case PlayerOneCriticalHitCombination[1]:
+                case PlayerOneCriticalHitCombination[2]:
+                    firstFighterCriticalAttackKeys[e.code] = true;
+
+                    if (fighterOneCriticalAttackReady() && !e.repeat && criticalAttackDelayOneEnd) {
+                        const damage = criticalHit(firstFighter);
+                        console.log(damage);
+                        secondFighterHealth -= damage;
+                        criticalAttackDelayOneEnd = false;
+                        setTimeout(() => {
+                            criticalAttackDelayOneEnd = true;
+                        }, 10000);
+                    }
+                    break;
+
+                case PlayerTwoCriticalHitCombination[0]:
+                case PlayerTwoCriticalHitCombination[1]:
+                case PlayerTwoCriticalHitCombination[2]:
+                    secondFighterCriticalAttackKeys[e.code] = true;
+                    if (fighterTwoCriticalAttackReady() && !e.repeat && criticalAttackDelayTwoEnd) {
+                        const damage = criticalHit(secondFighter);
+                        firstFighterHealth -= damage;
+                        criticalAttackDelayTwoEnd = false;
+                        setTimeout(() => {
+                            criticalAttackDelayTwoEnd = true;
+                        }, 10000);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            const leftHealhBarWidth = (firstFighterHealth / firstFighter.health) * 100;
+            const rightHealhBarWidth = (secondFighterHealth / secondFighter.health) * 100;
+
+            leftFighterIndicator.style.width = `${leftHealhBarWidth}%`;
+            rightFighterIndicator.style.width = `${rightHealhBarWidth}%`;
+
+            if (firstFighterHealth <= 0) {
+                leftFighterIndicator.style.width = '0%';
+                resolve(secondFighter);
+            } else if (secondFighterHealth <= 0) {
+                rightFighterIndicator.style.width = '0%';
+                resolve(firstFighter);
+            }
+        });
+
+        document.addEventListener('keyup', e => {
+            switch (e.code) {
+                case PlayerOneBlock:
+                    firstFighterBlockActive = false;
+                    break;
+
+                case PlayerTwoBlock:
+                    secondFighterBlockActive = false;
+                    break;
+
+                case PlayerOneCriticalHitCombination[0]:
+                case PlayerOneCriticalHitCombination[1]:
+                case PlayerOneCriticalHitCombination[2]:
+                    firstFighterCriticalAttackKeys[e.code] = false;
+                    break;
+
+                case PlayerTwoCriticalHitCombination[0]:
+                case PlayerTwoCriticalHitCombination[1]:
+                case PlayerTwoCriticalHitCombination[2]:
+                    secondFighterCriticalAttackKeys[e.code] = false;
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
 }
